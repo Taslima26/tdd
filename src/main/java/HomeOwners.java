566c62c214b188f8d6f099652c370f42fcbdef6d
taslima26 <banglawalas@hotmail.co.uk>
2021-04-23 21:45:36 +0100

    Initial setup done

---
+++ src/main/java/HomeOwners.java
@@ -0,0 +1,60 @@
+public class HomeOwners {
+    private String title;
+    private String firstName;
+    private String initial;
+
+    public HomeOwners(String title, String firstName, String initial, String lastName) {
+        this.title = title;
+        this.firstName = firstName;
+        this.initial = initial;
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+
+
+
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public void setInitial(String initial) {
+        this.initial = initial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @Override public String toString()
+    {
+        return "HomeOwner [title =" + title +",first-name=" + firstName + ", last-name=" + lastName + ", initial=" + initial + "]";
+    }
+
+
+
+
+
+}
--- src/main/java/ReadCSV.java
+++ src/main/java/ReadCSV.java
@@ -1,17 +1,56 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-public class ReadCSV
-{
-    public static void main(String[] args) throws Exception
-    {
-//parsing a CSV file into Scanner class constructor
 
-        Scanner sc = new Scanner(new File("src/main/resources/examples.csv"));
-        sc.useDelimiter(",");   //sets the delimiter pattern
-        while (sc.hasNext())  //returns a boolean value
-        {
-            System.out.print(sc.next());  //find and returns the next complete token from this scanner
+public class ReadCSV {
+
+
+    public static void main(String... args) {
+        List<HomeOwners> homeOwners = readHomeOwnerFromCSV("src/main/resources/examples.csv");
+        for (HomeOwners h : homeOwners) {
+            System.out.println(h);
         }
-        sc.close();  //closes the scanner
     }
+
+    public static List<HomeOwners> readHomeOwnerFromCSV(String fileName) {
+        List<HomeOwners> homeOwners = new ArrayList<>();
+        Path pathToFile = Paths.get(fileName);
+        try (BufferedReader br = Files.newBufferedReader(pathToFile,
+                StandardCharsets.US_ASCII)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] attributes = line.split(",");
+                HomeOwners homeOwner = createHomeowner(attributes);
+                homeOwners.add(homeOwner);
+                line = br.readLine();
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return homeOwners;
+    }
+
+    private static HomeOwners createHomeowner(String[] metadata) {
+
+
+        String firstName= metadata[0];
+        String title=metadata[0];
+        String inital=metadata[0];
+        String lastName=metadata[0];
+
+
+
+        return new HomeOwners(title,firstName,inital,lastName);
+    }
+
+
 }
+
